a485efeeed356ff59fdd5b8226899d26ae927dea
dave1496 <dav_elmaleh@hotmail.com>
2020-02-18 18:56:34 +0100

    feat: improve inference

--- inference.py
+++ inference.py
@@ -32,13 +32,14 @@ def load_model(model_path, patch_size):
 
 def inference(model, image, patch_size):
     mask = np.zeros(image.shape[:2])
-    for i in tqdm(range(0, image.shape[0], patch_size)):
-        for j in range(0, image.shape[1], patch_size):
+    for i in range(0, image.shape[0], patch_size):
+        print("Line {} / {}".format(i // patch_size, image.shape[0] // patch_size))
+        for j in tqdm(range(0, image.shape[1], patch_size)):
             patch_img = image[i:i+patch_size, j:j+patch_size]
             patch_img_shape = patch_img.shape[:2]
             patch_img = cv2.resize(patch_img, (patch_size, patch_size))
             patch_mask = model.predict(np.array([patch_img]), verbose=0)[0, :, :, 0]
-            mask[i:i+patch_img_shape[0], j:j+patch_img_shape[1]] = cv2.resize(patch_mask, patch_img_shape)
+            mask[i:i+patch_img_shape[0], j:j+patch_img_shape[1]] = cv2.resize(patch_mask, patch_img_shape[::-1])
     return mask
 
 ##########
@@ -50,4 +51,5 @@ if __name__ == "__main__":
     model = load_model(args.model_path, args.patch_size)
     image = cv2.imread(args.image_path) / 255.
     mask = inference(model, image, args.patch_size)
+    mask = (mask * 255).astype(np.uint8)
     cv2.imwrite(args.output_path, mask)
